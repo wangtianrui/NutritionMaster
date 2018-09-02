06e16847b074616b6578fe24171e96c2eecd9cf9
ScorpioMiku <1056992492@qq.com>
2018-09-02 14:47:43 +0800

    cardview

---
+++ app/src/main/java/com/example/ninefourone/nutritionmaster/adapter/CardHolder.java
@@ -0,0 +1,10 @@
+package com.example.ninefourone.nutritionmaster.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by ScorpioMiku on 2018/9/2.
+ */
+
+public class CardHolder extends RecyclerView.ViewHolder {
+}
