92f4b56f64ae9f1c6dcd3cbc9b42d1b37ad7e9de
ScorpioMiku <1056992492@qq.com>
2018-10-05 12:31:44 +0800

    1

---
+++ app/src/main/java/com/example/ninefourone/nutritionmaster/modules/classifyresult/DishResultActivity.java
@@ -0,0 +1,15 @@
+package com.example.ninefourone.nutritionmaster.modules.classifyresult;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ninefourone.nutritionmaster.R;
+
+public class DishResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dish_result);
+    }
+}
